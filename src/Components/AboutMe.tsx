487642323203e91152234a8d4133397311a6bf40
mbingram <74675664+mbingram@users.noreply.github.com>
2023-09-08 13:23:00 -0600

    updated copy and layouts

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -9,24 +9,24 @@ function AboutMe() {
                 <div className="relative bg-violet-950 py-8 pl-20 -ml-60 mb-5">
                     <h2 className="text-3xl font-semibold text-violet-50 absolute -mt-3 ml-40">A Bit More About Me</h2>
                 </div>
-                <p className="text-gray-600 mb-8">
+                <p className="mb-8">
                     I hope to utilize my technical skills and knowledge to make a positive impact on the world, using technology to solve real-world
                     problems. My aim is to create innovative and effective solutions that will improve people's lives and enhance their experiences,
                     and I am committed to working collaboratively with like-minded professionals to achieve our common goals.
                     <br />
-                    <p className="font-semibold">Ultimately, I aspire to be part of a team that is making a meaningful difference in the world.</p>
+                    <p className="font-semibold text-violet-950">Ultimately, I aspire to be part of a team that is making a meaningful difference in the world.</p>
                 </p>
                 <div className="flex flex-row justify-evenly mb-8">
                     <img src={art} alt="art" className="w-1/5 border border-violet-300 hover:border-pink-400 drop-shadow-lg rounded-md" />
                     <img src={art2} alt="art2" className="w-1/5 border border-violet-300 hover:border-pink-400 drop-shadow-lg rounded-md" />
                     <img src="https://via.placeholder.com/150" alt="Placeholder 3" className="w-1/5 border border-violet-300 hover:border-pink-400 drop-shadow-lg rounded-md" />
                 </div>
-                <p className="text-gray-600 mb-4">
+                <p className="mb-4">
                     I am an artist at heart, which directed me to choose Graphic Design as a major in college. While I do prefer to create art that is more
                     abstract, I am thankful for my digital design education, as it has been a huge asset during my web development journey. I love creating 
                     UX that is sleek, simple, and pleasant to interact with.
                 </p>
-                <p className="text-gray-600">
+                <p className="">
                     In addition to creating art, I love going on adventures with my dog, seeing live music, and consuming fiction in the form of books, television 
                     and movies. In my opinion, there's no better escape than immersing yourself in a carefully-curated fantasy world.
                 </p>
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,22 +3,27 @@ import headshot from '../Images/headshot.jpg';
 
 function Home() {
     return (
-        <div className="bg-gradient-to-r from-violet-950 to-violet-50 p-8 pt-10 w-screen flex flex-row justify-center">
-            <div className="w-2/3 h-2/3 py-20 px-10 mx-auto text-center flex flex-row bg-violet-50 drop-shadow-lg rounded-md border border-pink-400">
-                <div className="w-1/2 mr-20">
-                    <h1 className="text-4xl font-bold mb-6 text-violet-900">Hello! <br /> I'm Mary Beth.</h1>
-
-                    <p className="text-lg text-gray-600 leading-relaxed">
-                        Thanks for being here ✨ <br />
-                        <br />
-                        I am a full stack web developer with more than 2 years of experience, and a passion for front end programming. <br />
-                        Take a look around, and feel free to get in touch!
-                    </p>
+        <div className="bg-gradient-to-r from-violet-950 to-violet-50 p-8 pt-10 w-screen flex flex-row justify-center overflow-hidden">
+            <div className="w-5/6 py-5 px-10 mx-auto bg-violet-50 drop-shadow-lg rounded-md border border-pink-400">
+                <div className="relative bg-violet-950 py-8 pl-20 -ml-60 mb-5">
+                    <h2 className="text-3xl font-semibold text-violet-50 absolute -mt-3 ml-40">Hello! I'm Mary Beth,</h2>
                 </div>
-                <div className="w-1/2">
-                    <img className="border border-violet-300 drop-shadow-lg rounded-md" src={headshot} alt='headshot' />
+                <div className="flex flex-row w-full">
+                    <div className="w-1/2 mr-20 flex flex-col justify-center">
+                        <p className="text-lg text-gray-600 text-right leading-relaxed">
+                            a passionate software developer with over 2 years of professional experience.
+                            I specialize in creating clean, user-centric applications that not only function well but also look great.
+                            <br /> <br />
+                            I strive to make an impact through my work. My skill set ranges from full-stack development to a keen eye for design.
+                            This portfolio is a showcase of my journey and capabilities in the tech world.
+                            <br />
+                            Feel free to look around -- let's connect to build something extraordinary!
+                        </p>
+                    </div>
+                    <div className="w-1/2 flex flex-col justify-center">
+                        <img className="w-5/6 border border-violet-300 drop-shadow-lg rounded-md" src={headshot} alt='headshot' />
+                    </div>
                 </div>
-
             </div>
         </div>
     );
